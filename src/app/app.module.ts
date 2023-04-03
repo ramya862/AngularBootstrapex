4bcec91528658f5c0a1fcf8e16d684845cbf97bb
Ramya <adabalaramyajyothi@gmail.com>
2023-04-03 14:49:47 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
